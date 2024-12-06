b11ccb31c6885b154a95ca4d716382346db535e8
Daniel Collier <6226245+DanielJamesCollier@users.noreply.github.com>
2024-12-06 19:45:19 +0000

    day 3 part 1 done

---
+++ day_three/part_one/main.c
@@ -0,0 +1,60 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <Windows.h>  // For GetModuleFileName.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../../utils/djc.h"
+
+int main(void) {
+  size_t file_size = 0;
+  char* input_file_path =
+      djc_get_input_file("\\resources\\day_three\\input.txt");
+  char* file = djc_load_entire_file(input_file_path, &file_size);
+
+  if (file == NULL) {
+    printf("Failed to load the file input.txt: %s", input_file_path);
+    return 0;
+  }
+
+  char* current = file;
+  long int accumulator = 0;
+
+  while (*current) {
+    while (*current && *current != 'm')
+      current++;
+
+    current++;
+    if (*current == '\0') {
+      break;
+    }
+
+    if (*current == 'u') {
+      current++;
+      if (*current == 'l') {
+        current++;
+        if (*current == '(') {
+          current++;
+
+          char* end = NULL;
+          long int num_one = strtol(current, &end, 10);
+          if (*end != ',')
+            continue;
+          end++;
+          current = end;
+          long int num_two = strtol(current, &end, 10);
+          if (*end == ')') {
+            accumulator += num_one * num_two;
+          }
+          current = end++;
+        }
+      }
+    }
+    ++current;
+  }
+  printf("answer=%ld", accumulator);
+
+  return 0;
+}
--- debug.bat
+++ debug.bat
@@ -3,3 +3,5 @@ clang-cl /MDd /Zi /Od /Wall  -Wno-unsafe-buffer-usage -Wno-declaration-after-sta
 
 clang-cl /MDd /Zi /Od /Wall  -Wno-unsafe-buffer-usage -Wno-declaration-after-statement /std:c17 day_two/part_one/main.c -o out/day_2_part_1.exe
 clang-cl /MDd /Zi /Od /Wall  -Wno-unsafe-buffer-usage -Wno-declaration-after-statement /std:c17 day_two/part_two/main.c -o out/day_2_part_2.exe
+
+clang-cl /MDd /Zi /Od /Wall  -Wno-unsafe-buffer-usage -Wno-declaration-after-statement /std:c17 day_three/part_one/main.c -o out/day_3_part_1.exe
