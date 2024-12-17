72881c4d5af767472cca78717dd99b40153e0852
Daniel Collier <6226245+DanielJamesCollier@users.noreply.github.com>
2024-12-17 14:20:30 +0000

    use arenas in d1p2 and custom ints

--- 1/two.c
+++ 1/two.c
@@ -4,16 +4,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../djc_arena/djc_arena.h"
 #include "../utils/djc.h"
 
 internal int compare(const void* a, const void* b) {
-  return (*(const int*)a - *(const int*)b);
+  return (*(const s32*)a - *(const s32*)b);
 }
 
-int main(void) {
+s32 main(void) {
+  struct Arena* mem = arena_create("General");
   size_t file_size = 0;
   char* input_file_path = djc_get_input_file("resources\\day_one\\input.txt");
   char* file = djc_load_entire_file(input_file_path, &file_size);
+  djc_convert_crlf_to_lf(file);
 
   if (file == NULL) {
     printf("Failed to load the file input.txt: %s", input_file_path);
@@ -21,8 +24,9 @@ int main(void) {
   }
 
   size_t num_lines = djc_count_lines_in_file(file);
-  int* first_col = (int*)malloc(num_lines * sizeof(int));
-  int* second_col = (int*)malloc(num_lines * sizeof(int));
+  size_t num_bytes = num_lines * sizeof(s32);
+  s32* first_col = arena_alloc(mem, num_bytes);
+  s32* second_col = arena_alloc(mem, num_bytes);
 
   char* current = file;
   size_t i = 0;
@@ -42,13 +46,13 @@ int main(void) {
   }
 
   // sort the aray so we can early out. See "Fast early out"
-  qsort(second_col, num_lines, sizeof(int), compare);
+  qsort(second_col, num_lines, sizeof(s32), compare);
 
   for (i = 0; i < num_lines; i++) {
-    int left_current = first_col[i];
-    int count = 0;
+    s32 left_current = first_col[i];
+    s32 count = 0;
     for (size_t j = 0; j < num_lines; j++) {
-      int right_current = second_col[j];
+      s32 right_current = second_col[j];
 
       // Fast early out
       if (right_current > left_current) {
@@ -60,16 +64,14 @@ int main(void) {
     first_col[i] *= count;
   }
 
-  int total = 0;
+  s32 total = 0;
   for (i = 0; i < num_lines; i++) {
     total += first_col[i];
   }
 
   printf("Answer (d1p2) = %u\n", total);
 
-  free(first_col);
-  free(second_col);
   free(file);
-
+  arena_free(mem);
   return 0;
 }
