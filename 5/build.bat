1e45f48d249088a11f07e6b53fccecb6f3eb5b42
Daniel Collier <6226245+DanielJamesCollier@users.noreply.github.com>
2024-12-17 13:51:13 +0000

    fix build and ignore artifacts

--- 5/build.bat
+++ 5/build.bat
@@ -1,3 +1,3 @@
-set DEBUG_FLAGS=/permissive- /GS /W3 /Zc:wchar_t /ZI /Gm- /Od /sdl /Zc:inline /fp:precise  /errorReport:prompt /WX- /Zc:forScope /RTC1 /Gd /MDd /FC /EHsc /nologo /diagnostics:column /std:c17 /Fd../out /Fo../out
+set DEBUG_FLAGS=/permissive- /GS /W3 /Zc:wchar_t /ZI /Gm- /Od /sdl /Zc:inline /fp:precise  /errorReport:prompt /WX- /Zc:forScope /RTC1 /Gd /MDd /FC /EHsc /nologo /diagnostics:column /std:c17 /Fd../out
 
-cl /Fe../out/day_5_part_1.exe %DEBUG_FLAGS% one.c
+cl /Fe../out/day_5_part_1.exe %DEBUG_FLAGS% one.c ../djc_arena/djc_arena.c
