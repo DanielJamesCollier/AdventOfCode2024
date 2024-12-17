80db7878b228fef481fbf134eab4aa74c38fe66d
Daniel Collier <6226245+DanielJamesCollier@users.noreply.github.com>
2024-12-17 17:05:19 +0000

    user arenas in d2p2

--- 2/two.c
+++ 2/two.c
@@ -6,10 +6,11 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "../djc_arena/djc_arena.h"
 #include "../utils/djc.h"
 
 struct report {
-  int* level;
+  s32* level;
   size_t num_levels;
 };
 
@@ -18,7 +19,7 @@ struct reports {
   size_t num_reports;
 };
 
-internal void push_level(struct report* report, int level) {
+internal void push_level(struct report* report, s32 level) {
   assert(report);
 
   size_t new_num_levels = report->num_levels + 1;
@@ -34,16 +35,15 @@ internal void push_level(struct report* report, int level) {
   report->num_levels = new_num_levels;
 }
 
-internal bool is_safe(int* levels, size_t num_levels) {
+internal bool is_safe(s32* levels, size_t num_levels) {
   bool increasing = true;
   bool decreasing = true;
   for (size_t i = 0; i < num_levels - 1; i++) {
-
-    int diff = abs(levels[i] - levels[i + 1]);
+    s32 diff = abs(levels[i] - levels[i + 1]);
     if (diff < 1 || diff > 3) {
       return false;
     }
-     
+
     if (levels[i] >= levels[i + 1]) {
       increasing = false;
     }
@@ -56,12 +56,14 @@ internal bool is_safe(int* levels, size_t num_levels) {
     if (!increasing && !decreasing) {
       return false;
     }
-
   }
-  return increasing || decreasing; 
+  return increasing || decreasing;
 }
 
-internal int* remove_at(int* input_array, size_t input_array_length, size_t index_to_remove, size_t* new_array_length) { 
+internal s32* remove_at(int* input_array,
+                        size_t input_array_length,
+                        size_t index_to_remove,
+                        size_t* new_array_length) {
   size_t new_length = input_array_length - 1;
 
   int* new_array = (int*)malloc(new_length * sizeof(int));
@@ -74,77 +76,79 @@ internal int* remove_at(int* input_array, size_t input_array_length, size_t inde
     printf("index out of bounds in remove_at(). index=%zu", index_to_remove);
     exit(EXIT_FAILURE);
   }
- 
+
   for (size_t i = 0, j = 0; i < input_array_length; i++) {
-      if (index_to_remove != i) {
-          new_array[j++] = input_array[i];
-      }
+    if (index_to_remove != i) {
+      new_array[j++] = input_array[i];
+    }
   }
 
   *new_array_length = new_length;
   return new_array;
 }
 
-internal bool check(int* levels, size_t num_levels) {
+internal bool check(struct report* report) {
+  assert(report);
+  assert(report->level);
 
-  if (is_safe(levels, num_levels)) {
+  if (is_safe(report->level, report->num_levels)) {
     return true;
   }
 
-  for (size_t i = 0; i < num_levels; i++) {
-     size_t new_array_length = 0;
-     int* new_array = remove_at(levels, num_levels, i, &new_array_length); 
-     bool now_safe = is_safe(new_array, new_array_length);
-     free(new_array);
-     if (now_safe) {
-       return true;
-     }
+  for (size_t i = 0; i < report->num_levels; i++) {
+    size_t new_array_length = 0;
+    int* new_array =
+        remove_at(report->level, report->num_levels, i, &new_array_length);
+    bool now_safe = is_safe(new_array, new_array_length);
+    free(new_array);
+    if (now_safe) {
+      return true;
+    }
   }
   return false;
 }
 
-int main(void) {
+s32 main(void) {
+  struct Arena* mem = arena_create("General", 80);
   size_t file_size = 0;
   char* input_file_path = djc_get_input_file("\\resources\\day_two\\input.txt");
   char* file = djc_load_entire_file(input_file_path, &file_size);
+  djc_convert_crlf_to_lf(file);
 
   struct reports reports_list;
   reports_list.num_reports = djc_count_lines_in_file(file);
-  reports_list.rep =
-      (struct report*)calloc(reports_list.num_reports, sizeof(struct report));
+  reports_list.rep = (struct report*)arena_alloc(
+      mem, reports_list.num_reports * sizeof(struct report));
 
   if (file == NULL) {
     printf("Failed to load the file input.txt: %s", input_file_path);
     return 0;
   }
 
-  char* current = file;
   size_t i = 0;
+  for (char* line = file; line; line = get_next_line(line)) {
+    struct report* report = &reports_list.rep[i];
+    report->level = mem->current;
+    while (*line && *line != '\n') {
+      s32 current_level = atoi(line);
+      s32 digits = djc_count_digits(current_level);
 
-  while (*current != '\0') {
-    int current_level = atoi(current);
-    push_level(&reports_list.rep[i], current_level);
-    int digits = djc_count_digits(current_level);
-    current += digits;
+      arena_alloc(mem, sizeof(int));
+      report->level[report->num_levels++] = current_level;
 
-    if (*current == ' ') {
-      current++;
+      line += digits;
+      if (*line == ' ') {
+        line++;
+      }
     }
-
-    if (*current == '\r') {
-      current++;
-    }
-
-    if (*current == '\n') {
-      current++;
-      i++;
-    }
+    i++;
   }
 
   size_t num_safe_reports = 0;
 
   for (i = 0; i < reports_list.num_reports; i++) {
-    bool safe = check(reports_list.rep[i].level, reports_list.rep[i].num_levels);
+    struct report* report = &reports_list.rep[i];
+    bool safe = check(report);
 
     if (safe) {
       num_safe_reports++;
