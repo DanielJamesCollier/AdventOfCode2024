4f3f7b37c89d55436f1a20745a9e2c51b764170e
Daniel Collier <6226245+DanielJamesCollier@users.noreply.github.com>
2024-12-17 14:16:20 +0000

    use arena in day 1 part 1 and custom ints

--- 1/one.c
+++ 1/one.c
@@ -4,16 +4,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../djc_arena/djc_arena.h"
 #include "../utils/djc.h"
 
 internal int compare(const void* a, const void* b) {
-  return (*(const int*)a - *(const int*)b);
+  return (*(const s32*)a - *(const s32*)b);
 }
 
 int main(void) {
+  struct Arena* mem = arena_create("General");
   size_t file_size = 0;
   char* input_file_path = djc_get_input_file("\\resources\\day_one\\input.txt");
   char* file = djc_load_entire_file(input_file_path, &file_size);
+  djc_convert_crlf_to_lf(file);
 
   if (file == NULL) {
     printf("Failed to load the file input.txt: %s", input_file_path);
@@ -21,8 +24,10 @@ int main(void) {
   }
 
   size_t num_lines = djc_count_lines_in_file(file);
-  int* first_col = (int*)malloc(num_lines * sizeof(int));
-  int* second_col = (int*)malloc(num_lines * sizeof(int));
+  size_t num_bytes = num_lines * sizeof(s32);
+
+  s32* first_col = arena_alloc(mem, num_bytes);
+  s32* second_col = arena_alloc(mem, num_bytes);
 
   char* current = file;
   size_t i = 0;
@@ -41,10 +46,10 @@ int main(void) {
     i++;
   }
 
-  qsort(first_col, num_lines, sizeof(int), compare);
-  qsort(second_col, num_lines, sizeof(int), compare);
+  qsort(first_col, num_lines, sizeof(s32), compare);
+  qsort(second_col, num_lines, sizeof(s32), compare);
 
-  int total_distance = 0;
+  s32 total_distance = 0;
 
   for (i = 0; i < num_lines; i++) {
     total_distance += abs(first_col[i] - second_col[i]);
@@ -52,9 +57,7 @@ int main(void) {
 
   printf("Answer (d1p1) = %u\n", total_distance);
 
-  free(first_col);
-  free(second_col);
   free(file);
-
+  arena_free(mem);
   return 0;
 }
