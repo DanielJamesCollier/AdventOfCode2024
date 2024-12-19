a669ecc776d8b1fd1e25e47f3ff17a2f233dba8a
Daniel Collier <6226245+DanielJamesCollier@users.noreply.github.com>
2024-12-19 10:25:23 +0000

    Fix djc_count_lines_in_file to include final line.
    e.g. the last line in a file probably does not have \n but instead \0 only so we want to count it as a line.

--- utils/djc.h
+++ utils/djc.h
@@ -58,7 +58,7 @@ dedupe size_t djc_count_lines_in_file(const char* lines) {
 
   size_t num_lines = 0;
   while (*lines++) {
-    if (*lines == '\n') {
+    if (*lines == '\n' || *lines == '\0') {
       ++num_lines;
     }
   }
