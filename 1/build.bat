00fe63fcdd13bf4c37ca7ca24544a59642b61108
daniel.collier <6226245+DanielJamesCollier@users.noreply.github.com>
2025-01-01 21:58:53 +0000

    add linker flags for sanitizers to work

--- 1/build.bat
+++ 1/build.bat
@@ -1,3 +1,4 @@
-cl /Fe../out/day_1_part_1.exe %DEBUG_FLAGS% one.c ../djc_arena/djc_arena.c
-cl /Fe../out/day_1_part_2.exe %DEBUG_FLAGS% two.c ../djc_arena/djc_arena.c
+cl /Fe../out/day_1_part_1.exe %DEBUG_FLAGS% one.c ../djc_arena/djc_arena.c /link %LINKER_FLAGS%
+cl /Fe../out/day_1_part_2.exe %DEBUG_FLAGS% two.c ../djc_arena/djc_arena.c /link %LINKER_FLAGS%
+
 
---
+++ 7/build.bat
@@ -0,0 +1,2 @@
+cl /Fe../out/day_7_part_1.exe %DEBUG_FLAGS% one.c ../djc_arena/djc_arena.c /link %LINKER_FLAGS%
+
