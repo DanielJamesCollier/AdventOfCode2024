f088d69c6d7487d9d095fe1584706e2ae68bfa38
daniel.collier <6226245+DanielJamesCollier@users.noreply.github.com>
2025-01-02 11:22:12 +0000

    Fix incorrect exe name

--- 4/build.bat
+++ 4/build.bat
@@ -1,2 +1,2 @@
 cl /Fe../out/day_4_part_1.exe %DEBUG_FLAGS% one.c /link %LINKER_FLAGS%
-cl /Fe../out/day_5_part_2.exe %DEBUG_FLAGS% two.c /link %LINKER_FLAGS%
+cl /Fe../out/day_4_part_2.exe %DEBUG_FLAGS% two.c /link %LINKER_FLAGS%
