9213a312a8333f724babf454642e51f9e9c12825
daniel.collier <6226245+DanielJamesCollier@users.noreply.github.com>
2025-01-03 15:51:20 +0000

    Improve solution
    if chain to while loop

--- 3/one.c
+++ 3/one.c
@@ -6,6 +6,48 @@
 
 #include "../utils/djc.h"
 
+internal const char expected[] = "mul(";
+
+internal s64 solve(char* current) {
+  char* end = NULL;
+  size_t i = 0;
+  s64 num_one = 0;
+  s64 num_two = 0;
+  s64 accumulator = 0;
+
+  while (*current) {
+    i = 0;
+    while (expected[i] != '\0') {
+      if (*current == expected[i]) {
+        ++current;
+        ++i;
+      } else {
+        break;
+      }
+    }
+
+    if (i == 4) {
+      end = NULL;
+      num_one = strtol(current, &end, 10);
+      if (*end != ',')
+        continue;
+
+      ++end;
+      current = end;
+
+      num_two = strtol(current, &end, 10);
+
+      if (*end == ')') {
+        accumulator += num_one * num_two;
+      }
+      current = end;
+    } else {
+      ++current;
+    }
+  }
+  return accumulator;
+}
+
 s32 main(void) {
   size_t file_size = 0;
   char* input_file_path = djc_get_input_file("\\resources\\3\\input.txt");
@@ -17,50 +59,6 @@ s32 main(void) {
     return 0;
   }
 
-  char* current = file;
-  s64 accumulator = 0;
-
-  while (*current) {
-    while (*current && *current != 'm')
-      current++;
-
-    if (*current == '\0') {
-      break;
-    }
-
-    current++;
-
-    if (*current == 'u') {
-      current++;
-      if (*current == 'l') {
-        current++;
-        if (*current == '(') {
-          current++;
-          if (*current == '\0') {
-            break;
-          }
-
-          char* end = NULL;
-          s64 num_one = strtol(current, &end, 10);
-          if (*end != ',')
-            continue;
-
-          end++;
-          current = end;
-
-          long int num_two = strtol(current, &end, 10);
-          if (*end == ')') {
-            accumulator += num_one * num_two;
-          }
-          current = end++;
-        }
-      }
-    } else if (*current == '\0') {
-      break;
-    }
-    ++current;
-  }
-  printf("Answer (d3p1) = %I64d\n", accumulator);
-
+  printf("Answer (d3p1) = %I64d\n", solve(file));
   return 0;
 }
